d333ff881c0ec5558b49e8b5ab7ceb49a2ed5015
Max <33969759+MaXeraph@users.noreply.github.com>
2025-02-12 16:22:36 -0500

    change

--- pages/1_📈_Plotting_Demo.py
+++ pages/1_📈_Plotting_Demo.py
@@ -4,7 +4,7 @@ import numpy as np
 
 st.set_page_config(page_title="Plotting Demo", page_icon="📈")
 
-st.markdown("# Plotting Demo")
+st.markdown("# SUPER FAST DEMO")
 st.sidebar.header("Plotting Demo")
 st.write(
     """This demo illustrates a combination of plotting and animation with
